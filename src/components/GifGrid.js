095e219c413f95291201b67d5ce0a835cba97889
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2022-02-25 12:55:41 -0600

    MostrarTitulosImagenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,48 @@
 import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
     const [count, setCount] = useState(0)
+    const [images, setImages] = useState([]);
 
     useEffect( () => {
         getGifs();
     }, [])
-
     const getGifs = async () => {
-
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=w2gQILuCPAiMncgnww7l3NxbjUb2x7Yk';
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Timon+y+Pumba&limit=10&api_key=ihTGnMcTrJdgTETqjZQZ214kiYCEmhEf';
         const resp = await fetch( url );
         const { data } = await resp.json();
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
 
-        // export const GifGrid = ({category}) => {}
-
-    getGifs();
-
+        console.log(gifs);
+        setImages( gifs );
 
 
+    }
+  
   return (
     <div>
         <h3>{ category }</h3>
         <h3> {count} </h3>
         <button onClick={ () => setCount( count + 1 ) }></button>
+
+        {
+            images.map( img => (
+                <GifGridItem 
+                    key = { img.id }
+                    {...img } 
+
+                />
+            ))
+        }
+
     </div>
-  )}
+  )
 }
