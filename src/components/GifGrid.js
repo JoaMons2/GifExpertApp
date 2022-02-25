02a75415c7e244ff8c42ac29f07d15502be6565c
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2022-02-25 11:55:16 -0600

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export const GifGrid = ({ category }) => {
-    
-    const getGifs = async() => {
+export const GifGrid = ({category}) => {
 
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Timon+y+Pumba&limit=10&api_key=ihTGnMcTrJdgTETqjZQZ214kiYCEmhEf';
+    const [count, setCount] = useState(0)
+
+    useEffect( () => {
+        getGifs();
+    }, [])
+
+    const getGifs = async () => {
+
+
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=w2gQILuCPAiMncgnww7l3NxbjUb2x7Yk';
         const resp = await fetch( url );
         const { data } = await resp.json();
 
-        const gifs = data.map( img => {
-            return{
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-        
-        console.log(gifs);
-
-    }
+        // export const GifGrid = ({category}) => {}
 
     getGifs();
 
+
+
   return (
     <div>
-        <h3> { category } </h3>
+        <h3>{ category }</h3>
+        <h3> {count} </h3>
+        <button onClick={ () => setCount( count + 1 ) }></button>
     </div>
-  )
+  )}
 }
